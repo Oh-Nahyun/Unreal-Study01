78441e7e2bd07f9ecf9afdc2da76c1162d6e6689
Oh-Nahyun <annieonh@gmail.com>
2024-11-14 22:44:01 +0900

    Initial Commit

---
+++ CProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CProject
+//
+//  Created by 오나현 on 11/14/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
